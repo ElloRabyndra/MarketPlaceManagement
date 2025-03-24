b7c552e90f0d85ab107ca5195978b7516c1e7802
ElloRabyndra <ElloRabyndra123@gmail.com>
2025-03-24 15:34:03 +0700

    fix: fix upload icon

--- js/script.js
+++ js/script.js
@@ -1,13 +1,34 @@
-const popup = document.getElementById('popup');
-const addProductButton = document.getElementById('addProductButton');
-const closePopup = document.getElementById('closePopup');
+const popup = document.getElementById("popup");
+const addProductButton = document.getElementById("addProductButton");
+const closePopup = document.getElementById("closePopup");
+const uploadInput = document.getElementById("upload");
+const uploadIcon = document.getElementById("uploadIcon");
+const uploadLabel = document.querySelector("label[for='upload']");
 
 // Buka Popup Add Product
-addProductButton.addEventListener("click", function() {
-  popup.classList.replace("hidden", "flex");
-});
+if (addProductButton) {
+  addProductButton.addEventListener("click", function () {
+    popup.classList.replace("hidden", "flex");
+  });
+}
 
 // Tutup Popup Add Product
-closePopup.addEventListener("click", function() {
-  popup.classList.replace("flex", "hidden");
-});
+if (closePopup) {
+  closePopup.addEventListener("click", function () {
+    popup.classList.replace("flex", "hidden");
+    uploadIcon.classList.replace("text-green-500", "text-gray-400");
+    uploadIcon.classList.replace("bx-check", "bx-upload");
+    uploadLabel.classList.replace("text-green-500", "text-gray-400");
+  });
+}
+
+// Ganti Icon Upload
+if (uploadInput) {
+  uploadInput.addEventListener("change", function () {
+    if (uploadInput.files.length > 0) {
+      uploadIcon.classList.replace("text-gray-400", "text-green-500");
+      uploadIcon.classList.replace("bx-upload", "bx-check");
+      uploadLabel.classList.replace("text-gray-400", "text-green-500");
+    }
+  });
+}
