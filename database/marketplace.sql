f63fb4799ac7a206ec99dc9ec058c4d644f2b366
ElloRabyndra <Ellorabyndra123@gmail.com>
2025-05-18 16:48:50 +0700

    chore: update SQL file

--- database/marketplace.sql
+++ database/marketplace.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 08, 2025 at 04:02 AM
+-- Generation Time: May 16, 2025 at 03:11 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -40,13 +40,13 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `description`, `price`, `image`) VALUES
-(1, 'Camera Fuji Film', 'Kamera Untuk Foto', 1000000, 'c3cc531b73e6b0df5923a38c2ccbecd0f4a38932eee237d808eed7cca695ca87.jpeg'),
-(2, 'Lipstick', 'Lipstick Bibir', 150000, '7319f55c8c8bb6c6a4a667170ee7e7405059f6d7a9a063252367f69697817eb3.jpeg'),
+(1, 'Camera Fuji Film', 'Kamera Untuk Foto', 10000000, 'c3cc531b73e6b0df5923a38c2ccbecd0f4a38932eee237d808eed7cca695ca87.jpeg'),
+(2, 'Lipstick', 'Lipstick Untuk Bibir', 150000, '7319f55c8c8bb6c6a4a667170ee7e7405059f6d7a9a063252367f69697817eb3.jpeg'),
 (3, 'Thinkpad', 'Thinkpad Untuk Bekerja', 5000000, '139362c81cebc55c81feb312eb7dd9e6880c71bce2e06588878e070e3f31e636.jpeg'),
 (4, 'Jam Tangan', 'Jam Tangan Mewah', 4000000, '727cd8792e70d2f913e45a6a7103af16b8aa7f7e8d42dfc98ff5542e986ed982.png'),
 (5, 'Headset', 'Headset Gaming', 900000, 'db4591ec82be4ea475f4d152d7989fff855cc2b340fad952d969562499860572.jpeg'),
 (6, 'Sepatu', 'Sepatu Untuk Lari', 1500000, 'e364aa87c4f2d893b19fbd594ccb34c34154e5eb3bbc624243739f217b29ca7e.jpg'),
-(9, 'Pocari Sweat', 'Minuman ber-Ion', 8000, 'fd2b4ae26c575fb932d946fbab6eab286ed3615351132ab46cf3e7cd5d01a6d7.jpg'),
+(9, 'Pocari Sweat', 'Minuman Sehat ber-Ion', 8000, 'fd2b4ae26c575fb932d946fbab6eab286ed3615351132ab46cf3e7cd5d01a6d7.jpg'),
 (10, 'Mini Bite', 'Cemilan Enak &amp; Sehat', 10000, '74846a30e5a4416c42be60def2bab7838adddebd97bf551942715c4b6755b05b.png'),
 (27, 'Soundcore R50i', 'TWS Untuk Motoran', 195000, 'af5439985768bd2c83a2b024fde7778ef14edb2e13c62bc0b773920f6af10963.png');
 
@@ -61,6 +61,7 @@ CREATE TABLE `users` (
   `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `role` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'user',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -68,9 +69,10 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`) VALUES
-(6, 'ellorabyndra', 'ellorabyndra@gmail.com', '$2y$10$bFZEfRC/6jT1kplXF3EDpuraN7DQkoNBPJJ42cEQjrrbkJHAwzRLW', '2025-04-28 03:28:03'),
-(7, 'User', 'user@gmail.com', '$2y$10$pRgon66JPHXq0tt2fUL4M.qOxI/iGHueYza.QmPCo2IAWaFUgxoF6', '2025-04-29 16:06:41');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`, `created_at`) VALUES
+(6, 'ellorabyndra', 'ellorabyndra@gmail.com', '$2y$10$bFZEfRC/6jT1kplXF3EDpuraN7DQkoNBPJJ42cEQjrrbkJHAwzRLW', 'user', '2025-04-28 03:28:03'),
+(9, 'admin', 'admin@gmail.com', '$2y$10$.8opXLfpxD2/eWTyuL2CTuMClv70xLWIX6MvcxC7YHqpcLyQ108N.', 'admin', '2025-05-10 16:33:28'),
+(10, 'budiono', 'budiono@gmail.com', '$2y$10$DIgLjVlKdWFLeJefFyIUeOyHyOe7rYj7K906nIpz74L0u9o6Geg2m', 'user', '2025-05-15 17:39:08');
 
 --
 -- Indexes for dumped tables
@@ -98,13 +100,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
